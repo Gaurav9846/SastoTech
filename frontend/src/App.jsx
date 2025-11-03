c35030a74db655db27eb546bc0aa1b4e627e67f5
Gaurav9846 <gurunggaurav1611@gmail.com>
2025-11-03 12:39:59 +0545

    Initialized 1st frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Verify from './pages/Verify'
+import VerifyEmail from './pages/VerifyEmail'
+import Footer from './components/Footer'
+import Profile from './pages/Profile'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<><Navbar/><Home/><Footer/></>
+  },
+  {
+    path:'/signup',
+    element:<><Signup/></>
+  },
+  {
+    path:'/login',
+    element:<><Login/></>
+  },
+  {
+    path:'/verify',
+    element:<><Verify/></>
+  },
+  {
+    path:'/verify/:token',
+    element:<><VerifyEmail/></>
+  },
+  {
+    path:'/profile',
+    element:<><Navbar/><Profile/></>
+  },
+])
 
 const App = () => {
   return (
-    <div className='text-red-700'>
-      hello world
-    </div>
+    <>
+    <RouterProvider router={router}/>
+    </>
   )
 }
 
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { AppWindowIcon, CodeIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const Profile = () => {
+  return (
+    <div className="pt-20 min-h-screen bg-gray-100">
+      <Tabs defaultValue="profile" className="max-w-7xl mx-auto items-center">
+        <TabsList>
+          <TabsTrigger value="profile">Profile</TabsTrigger>
+          <TabsTrigger value="orders">Orders</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile">
+          <div>
+            <div className="flex flex-col justify-center items-center bg-gray-100">
+                <h1 className="font-bold mb-7 text-2xl text-gray-800">Update Profile</h1>
+                <div className="w-full flex gap-10 justify-between items-start px-7 max-w-2xl">
+                    {/* profile section  */}
+                    <div className="flex flex-col items-center">
+                        <img src="/gaurav.png" alt="profile" className="w-32 h-32 rounded-full object-cover border-4 border-pink-800" />
+                        <label className="mt-4 cursor-pointer bg-pink-600 text-white px-4 py-2 rounded-lg hover:bg-pink-700">Change Picture
+                            <input type="file" accept="image/*" className="hidden" />
+                        </label>
+                    </div>
+                    {/* profile form  */}
+                    <form action="" className="space-y-4 shadow-lg p-5 roundeed-lg bg-white">
+                        <div className="grid grid-cols-2 gap-4">
+                            <div>
+                                <label className="block text-sm font-medium">First Name</label>
+                                <Input type="text" 
+                                placeholder="John" name="firstName" className='w-full border rounded-lg px-3 py-2 mt-1' />
+                            </div>
+                            <div>
+                                <label className="block text-sm font-medium">Last Name</label>
+                                <Input type="text" 
+                                placeholder="Doe" name="lastName" className='w-full border rounded-lg px-3 py-2 mt-1' />
+                            </div>
+                        </div>
+                        <div>
+                              <label className="block text-sm font-medium">Email</label>
+                                <Input type="text" name="email" disabled className='w-full border rounded-lg px-3 py-2 mt-1 bg-gray-100 cursor-not-allowed' />
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium">Phone Number</label>
+                                <Input type="text" name="phoneNo" placeholder="Enter your Contact No" className='w-full border rounded-lg px-3 py-2 mt-1' />
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium">Address</label>
+                                <Input type="text" name="address" placeholder="Enter your Address" className='w-full border rounded-lg px-3 py-2 mt-1' />
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium">City</label>
+                                <Input type="text" name="city" placeholder="Enter your City" className='w-full border rounded-lg px-3 py-2 mt-1' />
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium">Zipcode</label>
+                                <Input type="text" name="zipcode" placeholder="Enter your Zipcode" className='w-full border rounded-lg px-3 py-2 mt-1' />
+                        </div>
+                        <Button type='submit' className='w-full mt-4 bg-pink-600 hover:bg-pink-700 text-white font-semibold py-2 rounded-lg'>
+                            Update Profile
+                        </Button>
+                    </form>
+                </div>
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="orders">
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, you&apos;ll be logged
+                out.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-current">Current password</Label>
+                <Input id="tabs-demo-current" type="password" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-new">New password</Label>
+                <Input id="tabs-demo-new" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Profile;
